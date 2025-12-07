89fa7199162e8bdcbce5bce1d9732a6aa8eeedfc
Jungmin Moon <jmmoon96@gmail.com>
2025-12-07 02:46:04 -0500

    Initial commit

---
+++ yugioh-collection-backend/src/main/java/com/github/jungmin_moon/yugioh_collection_backend/YugiohCardCollectionAndDecklistApplication.java
@@ -0,0 +1,13 @@
+package com.github.jungmin_moon.yugioh_collection_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohCardCollectionAndDecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohCardCollectionAndDecklistApplication.class, args);
+	}
+
+}
---
+++ yugioh-collection-backend/src/test/java/com/github/jungmin_moon/yugioh_collection_backend/YugiohCardCollectionAndDecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jungmin_moon.yugioh_collection_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugiohCardCollectionAndDecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
