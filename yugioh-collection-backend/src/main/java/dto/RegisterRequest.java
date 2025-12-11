469f484bfbbc34ec8f10a5c88bd05905e2fee4a6
Jungmin Moon <jmmoon96@gmail.com>
2025-12-11 05:01:33 -0500

    Work on registration

--- yugioh-collection-backend/src/main/java/dto/RegisterRequest.java
+++ yugioh-collection-backend/src/main/java/dto/RegisterRequest.java
@@ -1,14 +1,33 @@
 package dto;
 
-import org.springframework.stereotype.Service;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-import repositories.UserRepository;
-
-@Service
 public class RegisterRequest {
 
-	private UserRepository userRepository;
+	@NotNull
+	@Size(min = 5, max = 20)
+	private String username;
 	
+	@NotNull
+	@Size(min = 8, max = 20)
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	
 	
--- yugioh-collection-backend/src/main/java/services/UserRegistrationService.java
+++ yugioh-collection-backend/src/main/java/services/UserRegistrationService.java
@@ -1,12 +1,34 @@
 package services;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import dto.RegisterRequest;
+import entities.User;
+import repositories.UserRepository;
 
 @Service
 public class UserRegistrationService {
 
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	UserService userService;
+	UserRepository userRepository;
 	
-	public void registerUser() {
+	public void registerUser(RegisterRequest registerRequest) {
+		User user = new User();
+		
+		boolean alreadyExists = userService.doesUserAlreadyExist(registerRequest.getUsername());
+		
+		if (alreadyExists == true) {
+			System.out.println("You are already registered in the system.");
+		} else {
+		
+			user.setUsername(registerRequest.getUsername());
+			user.setPassword(bCryptPasswordEncoder.encode(registerRequest.getPassword()));
+			user.setAuthority("USER");
+			
+			userRepository.save(user);
+		}
 		
 	}
 }
