a9bce8491f2868ec262f9f6bd18682028e0f6d63
Jungmin Moon <jmmoon96@gmail.com>
2025-12-27 06:00:35 -0500

    Laying foundation for Deck endpoint

--- yugioh-collection-backend/src/main/java/com/github/jungmin_moon/yugioh_collection_backend/restcontroller/DeckController.java
+++ yugioh-collection-backend/src/main/java/com/github/jungmin_moon/yugioh_collection_backend/restcontroller/DeckController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.jungmin_moon.yugioh_collection_backend.repositories.DeckRepository;
+import com.github.jungmin_moon.yugioh_collection_backend.services.CardService;
 import com.github.jungmin_moon.yugioh_collection_backend.services.DeckService;
 
 @RestController
@@ -12,9 +13,11 @@ public class DeckController {
 
 	DeckService deckService;
 	DeckRepository deckRepository;
+	CardService cardService;
 	
-	DeckController(DeckService deckService, DeckRepository deckRepository) {
+	DeckController(DeckService deckService, DeckRepository deckRepository, CardService cardService) {
 		this.deckService = deckService;
 		this.deckRepository = deckRepository;
+		this.cardService = cardService;
 	}
 }
--- yugioh-collection-backend/src/main/java/com/github/jungmin_moon/yugioh_collection_backend/services/DeckService.java
+++ yugioh-collection-backend/src/main/java/com/github/jungmin_moon/yugioh_collection_backend/services/DeckService.java
@@ -1,5 +1,29 @@
 package com.github.jungmin_moon.yugioh_collection_backend.services;
 
+import org.springframework.stereotype.Service;
+
+import com.github.jungmin_moon.yugioh_collection_backend.repositories.DeckRepository;
+
+@Service
 public class DeckService {
 
+	DeckRepository deckRepository;
+	
+	DeckService(DeckRepository deckRepository) {
+		this.deckRepository = deckRepository;
+	}
+	
+	
+	
+	//Helper method to check user made sure to say card should be added to Main Deck or Side Deck, will make it so capitalization doesn't matter
+	public boolean validDeckLocation(String deckLocation) {
+	
+		boolean correctLocationSpelling = false;
+		
+		if (deckLocation.equalsIgnoreCase("main deck") || deckLocation.equalsIgnoreCase("side deck")) {
+			correctLocationSpelling = true;
+		}
+		
+		return correctLocationSpelling;
+	}
 }
