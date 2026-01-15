ca5e392811e28ccfd1db92ab0cbae06d8a7f7675
Jungmin Moon <jmmoon96@gmail.com>
2026-01-15 02:54:54 -0500

    Spring Docs helpful for controller tests

--- yugioh-collection-backend/src/test/java/com/github/jungmin_moon/yugioh_collection_backend/cardcontrollertests/CardRestControllerTests.java
+++ yugioh-collection-backend/src/test/java/com/github/jungmin_moon/yugioh_collection_backend/cardcontrollertests/CardRestControllerTests.java
@@ -1,24 +1,37 @@
 package com.github.jungmin_moon.yugioh_collection_backend.cardcontrollertests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
 import com.github.jungmin_moon.yugioh_collection_backend.restcontroller.CardController;
+import com.github.jungmin_moon.yugioh_collection_backend.services.CardService;
 
-@WebMvcTest(controllers = CardController.class)
+@WebMvcTest(CardController.class)
+@AutoConfigureRestTestClient
 public class CardRestControllerTests {
 
 	@Autowired
-	MockMvcTester mockMvcTester;
+	private RestTestClient restTestClient;
+	
+	@MockitoBean
+	private CardService cardService;
 	
 	@Test
-	void cardAdditionSuccessful() {
-		String requestBody = """
-				{
-					
-				}
-				""";
+	@DisplayName("Should return a 401 status because they are unauthorized")
+	void getAllShouldReturn401() {
+		
+		restTestClient.get().uri("/card")
+							.exchange()
+							.expectStatus()
+							.isEqualTo(HttpStatus.UNAUTHORIZED);
+		
 	}
+	
+	
 }
