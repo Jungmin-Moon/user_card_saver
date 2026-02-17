849ea1bf70550b5f6516edb756f5d66f2f89d472
Jungmin Moon <jmmoon96@gmail.com>
2026-02-17 13:39:40 -0500

    Setting up template

--- yugioh-collection-backend/src/test/java/com/github/jungmin_moon/yugioh_collection_backend/cardcontrollertests/CardRestControllerUpdateCardQuantityTests.java
+++ yugioh-collection-backend/src/test/java/com/github/jungmin_moon/yugioh_collection_backend/cardcontrollertests/CardRestControllerUpdateCardQuantityTests.java
@@ -1,13 +1,21 @@
 package com.github.jungmin_moon.yugioh_collection_backend.cardcontrollertests;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import com.github.jungmin_moon.yugioh_collection_backend.entities.Card;
 import com.github.jungmin_moon.yugioh_collection_backend.restcontroller.CardController;
@@ -36,7 +44,24 @@ public class CardRestControllerUpdateCardQuantityTests {
 	
 	@Test
 	@DisplayName("Test to make sure that users without authorization can't access the endpoint.")
-	void givenNoAuthorization_WhenAccessingEndpoint_Return4XX() {
+	void givenNoAuthorization_WhenAccessingEndpoint_Return4XX() throws Exception {
+		
+		String requestBody = """
+				{
+					"cardName": "Dark Magician",
+					"quantity": 5
+				}
+				""";
+		
+		ResultActions result = mvc.perform(MockMvcRequestBuilders
+											.put("/card/update/quantity")
+											.with(csrf())
+											.contentType(MediaType.APPLICATION_JSON)
+											.content(requestBody)
+											.accept(MediaType.APPLICATION_JSON));
+		
+		result.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().is4xxClientError());
 		
 	}
 	
@@ -44,6 +69,9 @@ public class CardRestControllerUpdateCardQuantityTests {
 	@DisplayName("Test to make sure does not exist message appears when user is authorized and they don't own the card.")
 	@WithMockUser(username = "testUser3", password = "password1", roles = {"USER"})
 	void givenUpdateCardQuantityRequest_WhenCardNotExist_ThenReturnNotInCollectionMessage() {
+		
+		when(cardService.isCardInDatabase("testUser3", "Dark Magician")).thenReturn(true);
+		
 		
 	}
 	
